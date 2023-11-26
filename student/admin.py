7af0ff8a501aa21d42131fd4a7bb834fd5e28c15
jishnu pr <ijishnupr@gmail.com>
2023-11-26 23:23:17 +0530

    with out choice on admin

--- student/admin.py
+++ student/admin.py
@@ -1,13 +1,4 @@
 from django.contrib import admin
-from .models import Questions,Category,Choice
+from .models import Questions,Category
 
-
-class ChoiceInline(admin.TabularInline):
-    model = Choice
-    extra = 3  # Number of choices displayed by default
-
-class QuestionsAdmin(admin.ModelAdmin):
-    inlines = [ChoiceInline]
-
-admin.site.register(Questions, QuestionsAdmin)
 
